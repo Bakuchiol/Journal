cc64e1412fd027ac78fa29e5ce5f0a8691a0ba9f
Lynette Cargo <lynettecargo@gmail.com>
2023-09-05 10:41:40 -0500

    new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate, Link} from 'react-router-dom'
 import { getUser } from './utilities/users-service';
 import AuthPage from './pages/AuthPage/AuthPage'
 import MainPage from './pages/MainPage/MainPage';
@@ -8,6 +8,7 @@ import './App.css';
 import Board from './components/Board/Board';
 import CreateEntry from './components/CreateEntry/CreateEntry';
 import BoardPage from './pages/BoardPage/BoardPage';
+import NoPage from './pages/NoPage/NoPage';
 
 function App() {
   const [user, setUser] = useState(getUser());
@@ -25,8 +26,10 @@ function App() {
             <Route path="/entries" element={<CreateEntry user={user} setUser={setUser} entry={entry} setEntry={setEntry}/>} />
             <Route exact path="/board" element={<Board photos={photos} user={user} setUser={setUser}/>} />
             <Route path="/board/mood" element={<BoardPage />} />
+            <Route path="*" element={<NoPage />} />
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
             {/* <Route path="/*" element={<Navigate to="/" />} /> */}
+            <Route path="/*" element={<h3>Lavender Journal <Link to="/entries">Lavender Journal</Link></h3>} />
           </Routes>
         </>
         :
